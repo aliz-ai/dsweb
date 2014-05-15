6fd7cf23b2425302f27e40765b6b5db1006aeb18
David Ipacs <david.ipacs@gmail.com>
2014-05-15 13:44:48 +0200

    Deleted unused renderers, and Cell added to factories.

--- dsweb/src/main/java/com/doctusoft/dsw/client/comp/UIObjectFactory.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/comp/UIObjectFactory.java
@@ -2,6 +2,7 @@ package com.doctusoft.dsw.client.comp;
 
 import com.doctusoft.bean.ModelObject;
 import com.doctusoft.dsw.client.comp.model.ButtonModel;
+import com.doctusoft.dsw.client.comp.model.CellModel;
 import com.doctusoft.dsw.client.comp.model.CheckboxModel;
 import com.doctusoft.dsw.client.comp.model.ContainerModel;
 import com.doctusoft.dsw.client.comp.model.HistoryHandlerModel;
@@ -15,34 +16,49 @@ import com.doctusoft.dsw.client.comp.model.TextareaModel;
 import com.doctusoft.synchronic.serialization.ObjectModelFactory;
 
 public class UIObjectFactory implements ObjectModelFactory {
-
+	
 	@Override
 	public ModelObject createModelObject(String className) {
-		if (ButtonModel.class.getName().equals(className))
+		if (ButtonModel.class.getName().equals(className)) {
 			return new ButtonModel();
-		if (LabelModel.class.getName().equals(className))
+		}
+		if (LabelModel.class.getName().equals(className)) {
 			return new LabelModel();
-		if (ContainerModel.class.getName().equals(className))
+		}
+		if (ContainerModel.class.getName().equals(className)) {
 			return new ContainerModel();
-		if (InputTextModel.class.getName().equals(className))
+		}
+		if (InputTextModel.class.getName().equals(className)) {
 			return new InputTextModel();
-		if (SelectModel.class.getName().equals(className))
+		}
+		if (SelectModel.class.getName().equals(className)) {
 			return new SelectModel();
-		if (SelectItemModel.class.getName().equals(className))
+		}
+		if (SelectItemModel.class.getName().equals(className)) {
 			return new SelectItemModel();
-		if (LinkModel.class.getName().equals(className))
+		}
+		if (LinkModel.class.getName().equals(className)) {
 			return new LinkModel();
-		if (HistoryHandlerModel.class.getName().equals(className))
+		}
+		if (HistoryHandlerModel.class.getName().equals(className)) {
 			return new HistoryHandlerModel();
-		if (ModalDialogModel.class.getName().equals(className))
+		}
+		if (ModalDialogModel.class.getName().equals(className)) {
 			return new ModalDialogModel();
-		if (CheckboxModel.class.getName().equals(className))
+		}
+		if (CheckboxModel.class.getName().equals(className)) {
 			return new CheckboxModel();
-		if (TextareaModel.class.getName().equals(className))
+		}
+		if (TextareaModel.class.getName().equals(className)) {
 			return new TextareaModel();
-		if (InplaceText.class.getName().equals(className))
+		}
+		if (InplaceText.class.getName().equals(className)) {
 			return new ContainerModel();
+		}
+		if (CellModel.class.getName().equals(className)) {
+			return new CellModel();
+		}
 		return null;
 	}
-
+	
 }
--- dsweb/src/main/java/com/doctusoft/dsw/client/gwt/RendererFactoryImpl.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/gwt/RendererFactoryImpl.java
@@ -3,6 +3,7 @@ package com.doctusoft.dsw.client.gwt;
 import com.doctusoft.dsw.client.AbstractRendererFactory;
 import com.doctusoft.dsw.client.comp.model.BaseComponentModel;
 import com.doctusoft.dsw.client.comp.model.ButtonModel;
+import com.doctusoft.dsw.client.comp.model.CellModel;
 import com.doctusoft.dsw.client.comp.model.CheckboxModel;
 import com.doctusoft.dsw.client.comp.model.ContainerModel;
 import com.doctusoft.dsw.client.comp.model.HistoryHandlerModel;
@@ -24,7 +25,7 @@ public class RendererFactoryImpl extends AbstractRendererFactory {
 			return new ButtonRenderer((ButtonModel) baseWidget);
 		}
 		if (baseWidget instanceof InputTextModel) {
-			return new InputTextRenderer((InputTextModel) baseWidget); 
+			return new InputTextRenderer((InputTextModel) baseWidget);
 		}
 		if (baseWidget instanceof ContainerModel) {
 			return new ContainerRenderer((ContainerModel) baseWidget);
@@ -32,16 +33,25 @@ public class RendererFactoryImpl extends AbstractRendererFactory {
 		if (baseWidget instanceof SelectModel) {
 			return new SelectRenderer((SelectModel) baseWidget);
 		}
-		if (baseWidget instanceof LinkModel)
+		if (baseWidget instanceof LinkModel) {
 			return new LinkRenderer((LinkModel) baseWidget);
-		if (baseWidget instanceof HistoryHandlerModel)
+		}
+		if (baseWidget instanceof HistoryHandlerModel) {
 			return new HistoryHandlerRenderer((HistoryHandlerModel) baseWidget);
-		if (baseWidget instanceof ModalDialogModel)
+		}
+		if (baseWidget instanceof ModalDialogModel) {
 			return new ModalDialogRenderer((ModalDialogModel) baseWidget);
-		if (baseWidget instanceof CheckboxModel)
+		}
+		if (baseWidget instanceof CheckboxModel) {
 			return new CheckboxRenderer((CheckboxModel) baseWidget);
-		if (baseWidget instanceof TextareaModel)
+		}
+		if (baseWidget instanceof TextareaModel) {
 			return new TextareaRenderer((TextareaModel) baseWidget);
+		}
+		if (baseWidget instanceof CellModel) {
+			return new CellRenderer((CellModel) baseWidget);
+		}
+		
 		return null;
 	}
 	
