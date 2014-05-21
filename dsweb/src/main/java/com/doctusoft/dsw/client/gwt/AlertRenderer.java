6f4c3b4de5123a84a22b8567e6a97c835b4e346f
peter.farkas@doctusoft.com <peter.farkas@doctusoft.com>
2014-05-21 15:20:07 +0200

    fixcubed

--- dsweb/src/main/java/com/doctusoft/dsw/client/gwt/AlertRenderer.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/gwt/AlertRenderer.java
@@ -31,7 +31,7 @@ public class AlertRenderer extends ContainerRenderer {
 				widget.clone(false).children().remove().end().text(newValue);
 			}
 		});
-		JQuery.select(widget.children().get(0)).attr("data-dismiss", "alert");
+		widget.find(".close").attr("data-dismiss", "alert");
 	}
 	
 }
--- dsweb/src/main/java/com/doctusoft/dsw/client/gwt/DropdownButtonRenderer.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/gwt/DropdownButtonRenderer.java
@@ -1,14 +1,13 @@
 package com.doctusoft.dsw.client.gwt;
 
 import com.doctusoft.dsw.client.comp.model.DropdownButtonModel;
-import com.xedge.jquery.client.JQuery;
 
 public class DropdownButtonRenderer extends ContainerRenderer {
 
 	public DropdownButtonRenderer(DropdownButtonModel container) {
 		super(container);
-		JQuery.select(widget.children().get(0)).append("<span class=\"caret\"></span>");
-		JQuery.select(widget.children().get(0)).attr("data-toggle", "dropdown"); 
+		widget.find(".dropdown-toggle").append("<span class=\"caret\"></span>");
+		widget.find(".dropdown-toggle").attr("data-toggle", "dropdown"); 
 	}
 	
 }
