01b2dccd3ac02d93f5697ac128065576c6e17573
gabor-farkas <gabor.farkas@doctusoft.com>
2014-05-22 10:44:30 +0200

    builder stuff on Cell

--- dsweb/src/main/java/com/doctusoft/dsw/client/comp/Cell.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/comp/Cell.java
@@ -10,6 +10,24 @@ public class Cell extends AbstractContainer<Cell, CellModel> {
 	public Cell() {
 		super(new CellModel());
 	}
+	
+	public void setSpan(int span) {
+		model.setSpan(span);
+	}
+	
+	public void setOffset(int offset) {
+		model.setOffset(offset);
+	}
+	
+	public Cell withSpan(int span) {
+		setSpan(span);
+		return this;
+	}
+	
+	public Cell withOffset(int offset) {
+		setOffset(offset);
+		return this;
+	}
     
     public Cell bindSpan(final ValueBinding<Integer> spanBinding) {
 		Bindings.bind(spanBinding, Bindings.obs(model).get(CellModel_._span));
