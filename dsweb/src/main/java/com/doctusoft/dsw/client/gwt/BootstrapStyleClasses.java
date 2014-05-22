3632f056404e0af62191f4b874b20a6171a66096
David Ipacs <david.ipacs@gmail.com>
2014-05-22 11:15:14 +0200

    Bootstrap icon classes added.

--- dsweb/src/main/java/com/doctusoft/dsw/client/gwt/BootstrapStyleClasses.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/gwt/BootstrapStyleClasses.java
@@ -7,44 +7,63 @@
 package com.doctusoft.dsw.client.gwt;
 
 /**
- *
+ * 
  * @author dipacs
  */
 public class BootstrapStyleClasses {
-    
-    // containers
-    public static final String ROW = "row";
-    public static final String ROW_FLUID = "row-fluid";
-    public static final String CONTAINER = "container";
-    public static final String CONTAINER_FLUID = "container-fluid";
-     
-    // buttons
-    public static final String BTN = "btn";
-    public static final String BTN_PRIMARY = "btn-primary";
-    public static final String BTN_INFO = "btn-info";
-    public static final String BTN_SUCCESS = "btn-success";
-    public static final String BTN_WARNING = "btn-warning";
-    public static final String BTN_DANGER = "btn-danger";
-    public static final String BTN_INVERSE = "btn-inverse";
-    public static final String BTN_LINK = "btn-link";
-     
-    // button sizes
-    public static final String BTN_MINI = "btn-mini";
-    public static final String BTN_SMALL = "btn-small";
-    public static final String BTN_LARGE = "btn-large";
-    
-    // common
-    public static final String DISABLED = "disabled";
-    
-    // responsive utility classes
-    public static final String VISIBLE_PHONE = "visible-phone";
-    public static final String VISIBLE_TABLET = "visible-tablet";
-    public static final String VISIBLE_DESKTOP = "visible-desktop";
-    public static final String HIDDEN_PHONE = "hidden-phone";
-    public static final String HIDDEN_TABLET = "hidden-tablet";
-    public static final String HIDDEN_DESKTOP = "hidden-desktop";
-
-    private BootstrapStyleClasses() {
-    }
-    
+	
+	// containers
+	public static final String ROW = "row";
+	public static final String ROW_FLUID = "row-fluid";
+	public static final String CONTAINER = "container";
+	public static final String CONTAINER_FLUID = "container-fluid";
+	
+	// buttons
+	public static final String BTN = "btn";
+	public static final String BTN_PRIMARY = "btn-primary";
+	public static final String BTN_INFO = "btn-info";
+	public static final String BTN_SUCCESS = "btn-success";
+	public static final String BTN_WARNING = "btn-warning";
+	public static final String BTN_DANGER = "btn-danger";
+	public static final String BTN_INVERSE = "btn-inverse";
+	public static final String BTN_LINK = "btn-link";
+	
+	// button sizes
+	public static final String BTN_MINI = "btn-mini";
+	public static final String BTN_SMALL = "btn-small";
+	public static final String BTN_LARGE = "btn-large";
+	
+	// table
+	public static final String TABLE = "table";
+	public static final String TABLE_STRIPPED = "table-stripped";
+	public static final String TABLE_BORDERED = "table-bordered";
+	public static final String TABLE_HOVER = "table-hover";
+	public static final String TABLE_CONDENSED = "table-condensed";
+	
+	// images
+	public static final String IMG_ROUNDED = "table";
+	public static final String IMG_CIRCLE = "table";
+	public static final String IMG_POLAROID = "table";
+	
+	// icon
+	public static final String ICON_WHITE = "icon-white";
+	
+	// common
+	public static final String DISABLED = "disabled";
+	public static final String SUCCESS = "success";
+	public static final String ERROR = "error";
+	public static final String WARNING = "warning";
+	public static final String INFO = "info";
+	
+	// responsive utility classes
+	public static final String VISIBLE_PHONE = "visible-phone";
+	public static final String VISIBLE_TABLET = "visible-tablet";
+	public static final String VISIBLE_DESKTOP = "visible-desktop";
+	public static final String HIDDEN_PHONE = "hidden-phone";
+	public static final String HIDDEN_TABLET = "hidden-tablet";
+	public static final String HIDDEN_DESKTOP = "hidden-desktop";
+	
+	private BootstrapStyleClasses() {
+	}
+	
 }
