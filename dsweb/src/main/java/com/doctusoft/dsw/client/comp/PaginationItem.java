75750d0b8ef09ab03962cd75042e37f77033151b
gabor-farkas <gabor.farkas@doctusoft.com>
2014-05-22 10:51:59 +0200

    lombok workaround

--- dsweb/src/main/java/com/doctusoft/dsw/client/comp/PaginationItem.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/comp/PaginationItem.java
@@ -1,12 +1,13 @@
 package com.doctusoft.dsw.client.comp;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import com.doctusoft.ObservableProperty;
 
-@Data
 public class PaginationItem {
-	
+
+	@Getter @Setter
 	private int seq; 
 	
 	@ObservableProperty
@@ -15,6 +16,7 @@ public class PaginationItem {
 	@ObservableProperty
 	private Boolean active;
 	
+	@Getter @Setter
 	private String link = "#";
 
 }
