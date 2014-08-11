429dcf04c0321551c0e465ae64041d2be1993ce4
Erhard Pfisztner <erhard.pfisztner@doctusoft.com>
2014-08-11 20:04:05 +0200

    ContextMenu updated

--- dsweb-example/src/main/java/com/doctusoft/dsw/sample/client/person/SandboxView.java
+++ dsweb-example/src/main/java/com/doctusoft/dsw/sample/client/person/SandboxView.java
@@ -1,4 +1,3 @@
-
 package com.doctusoft.dsw.sample.client.person;
 
 import com.doctusoft.dsw.client.comp.Button;
@@ -14,39 +13,36 @@ import com.doctusoft.dsw.client.comp.datatable.DateFormatter;
 import com.doctusoft.dsw.client.comp.mvp.ContainerWithPresenter;
 
 public class SandboxView extends ContainerWithPresenter<SandboxActivity> {
-	
-	private Select<String> select;
-	private InputTags inputTagsWithOption;
-	private InputText focus;
-
-	public SandboxView() {
-		new InputText().appendTo(container).addStyleClass("valami");
-		
-		
-		new DataTable<PersonDto>()
-		.addColumn(Columns.from("Id", PersonDto_._id))
-		.addColumn(Columns.obs("Name", PersonDto_._name))
-		.addColumn(Columns.from("Email", PersonDto_._email))
-		.addColumn(Columns.from("Born", PersonDto_._birthDate).format(new DateFormatter("yyyy-MM-dd")))
-		.bind(bindOnPresenter().get(SandboxActivity_._personList))
-		.bindSelectionMode(bindOnPresenter().get(SandboxActivity_._selectionMode))
-		.bindSelection(bindOnPresenter().get(SandboxActivity_._selection))
-		.appendTo(container).addStyleClass("valami");
-		
-		
-		new ContextMenu<Link>()
-		.addMenuItem(new Link("Kijelölt törlése").click(presenterMethod(SandboxActivity_.__hideLabel)))
-		.setConnectedObjectSelector("td")
-		.appendTo(container).addStyleClass("valami2");
-		
-		new Label("hide me").bindVisible(bindOnPresenter().get(SandboxActivity_._visibility)).appendTo(container);
-		new Button("új").click(presenterMethod(SandboxActivity_.__hideLabel)).appendTo(container);
-	}
-	
-	@Override
-	public void viewPresented() {
+  
+  private Select<String> select;
+  private InputTags inputTagsWithOption;
+  private InputText focus;
+  
+  public SandboxView() {
+    new InputText().appendTo(container).addStyleClass("valami");
+    
+    new DataTable<PersonDto>()
+      .addColumn(Columns.from("Id", PersonDto_._id))
+      .addColumn(Columns.obs("Name", PersonDto_._name))
+      .addColumn(Columns.from("Email", PersonDto_._email))
+      .addColumn(Columns.from("Born", PersonDto_._birthDate).format(new DateFormatter("yyyy-MM-dd")))
+      .bind(bindOnPresenter().get(SandboxActivity_._personList))
+      .bindSelectionMode(bindOnPresenter().get(SandboxActivity_._selectionMode))
+      .bindSelection(bindOnPresenter().get(SandboxActivity_._selection))
+      .appendTo(container).addStyleClass("valami");
+    
+    new ContextMenu<Link>("td", "valami2")
+      .addMenuItem(new Link("Kijelölt törlése").click(presenterMethod(SandboxActivity_.__hideLabel)))
+      //.setConnectedObjectSelector("td")
+      .appendTo(container);//.addStyleClass("valami2");
+    
+    new Label("hide me").bindVisible(bindOnPresenter().get(SandboxActivity_._visibility)).appendTo(container);
+    new Button("új").click(presenterMethod(SandboxActivity_.__hideLabel)).appendTo(container);
+  }
+  
+  @Override public void viewPresented() {
 //		focus.focus();
 //		select.setSelectItems(getPresenter().getLocationItems());
-	}
-	
+  }
+  
 }
--- dsweb/src/main/java/com/doctusoft/dsw/client/comp/ContextMenu.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/comp/ContextMenu.java
@@ -2,33 +2,32 @@ package com.doctusoft.dsw.client.comp;
 
 import com.doctusoft.dsw.client.comp.model.ContextMenuModel;
 
-
-public class ContextMenu<Item extends BaseComponent> extends AbstractContainer<ContextMenu, ContextMenuModel>{
-
-	public ContextMenu() {
-		super(new ContextMenuModel());
-		model.setElementType("ul id='contextMenu' class='dropdown-menu' role='menu'");
-		this.setStyle("display:none");
-	}
-	
-	public ContextMenu setConnectedObjectSelector(String objectClass) {
-		this.getModel().setConnectedObjectId(objectClass);
-		return this;
-	}
-
-	public ContextMenu addMenuItem(Item item) {
-		item.appendTo(new BaseContainer("li").appendTo(this));
-		return this;
-	}
-	
-	@Override
-	public void addStyleClass(String styleClass) {
-		this.getModel().setSelector(styleClass);
-		super.addStyleClass(styleClass);
-	}
-	
-	@Override
-	public ContextMenu appendTo(IsContainer container) {
-		return super.appendTo(container);
-	}
+public class ContextMenu<Item extends BaseComponent> extends AbstractContainer<ContextMenu, ContextMenuModel> {
+  
+  public ContextMenu(String objectClass, String selector) {
+    super(new ContextMenuModel());
+    model.setElementType("ul id='contextMenu' class='dropdown-menu' role='menu'");
+    this.setStyle("display:none");
+    this.setConnectedObjectSelector(objectClass);
+    this.addStyleClass(selector);
+  }
+  
+  public ContextMenu setConnectedObjectSelector(String objectClass) {
+    this.getModel().setConnectedObjectId(objectClass);
+    return this;
+  }
+  
+  public ContextMenu addMenuItem(Item item) {
+    item.appendTo(new BaseContainer("li").appendTo(this));
+    return this;
+  }
+  
+  @Override public void addStyleClass(String styleClass) {
+    this.getModel().setSelector(styleClass);
+    super.addStyleClass(styleClass);
+  }
+  
+  @Override public ContextMenu appendTo(IsContainer container) {
+    return super.appendTo(container);
+  }
 }
