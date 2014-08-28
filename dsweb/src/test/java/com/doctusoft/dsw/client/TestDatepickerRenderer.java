778e396c59febbb4b7c2f74daf4dc1867bcb7796
mborbely <mate.borbely@doctusoft.com>
2014-08-28 19:32:50 +0200

    component renderer tests

---
+++ dsweb/src/test/java/com/doctusoft/dsw/client/TestDatepickerRenderer.java
@@ -0,0 +1,25 @@
+
+package com.doctusoft.dsw.client;
+
+import java.text.ParseException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TestDatepickerRenderer extends AbstractDswebTest {
+	
+	@Test
+	@Ignore
+	//TODO vmiert elszall a javascript a komponens inicializalasakor
+	public void testFormat() throws ParseException {
+		//		Datepicker datePicker = new Datepicker().withId( "datepicker" );
+		//		registerApp( datePicker );
+		//		JQuery jqDatepicker = JQuery.select( "#datepicker" );
+		//		jqDatepicker.val( "2014.02.22" );
+		//		jqDatepicker.change();
+		//		Date modelDate = datePicker.getModel().getValue();
+		//		assertEquals( 2014, modelDate.getYear() );
+		//		assertEquals( 1, modelDate.getMonth() );
+		//		assertEquals( 22, modelDate.getDay() );
+	}
+}
