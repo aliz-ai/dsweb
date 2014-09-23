52d8b5c775f0bc2279a2a73dca657560ebff2b4e
gabor-farkas <gabor.farkas@doctusoft.com>
2014-09-23 16:08:28 +0200

    button rendering null fix

--- dsweb/src/main/java/com/doctusoft/dsw/client/gwt/ButtonRenderer.java
+++ dsweb/src/main/java/com/doctusoft/dsw/client/gwt/ButtonRenderer.java
@@ -24,23 +24,22 @@ package com.doctusoft.dsw.client.gwt;
 
 
 import com.doctusoft.bean.ValueChangeListener;
-import com.doctusoft.bean.binding.Bindings;
 import com.doctusoft.dsw.client.comp.model.ButtonModel;
 import com.doctusoft.dsw.client.comp.model.ButtonModel_;
+import com.google.common.base.Objects;
 import com.xedge.jquery.client.JQuery;
 
 public class ButtonRenderer extends BaseComponentRenderer {
 	
 	public ButtonRenderer(final ButtonModel button) {
 		super(JQuery.select("<button/>"), button);
-		widget.text(button.getCaption());
 		widget.addClass("btn");
-		Bindings.obs(button).get(ButtonModel_._caption).addValueChangeListener(new ValueChangeListener<String>() {
+		addChangeListenerAndApply(ButtonModel_._caption, button, new ValueChangeListener<String>() {
 			@Override
 			public void valueChanged(String newValue) {
-				widget.text(newValue);
+				widget.text(Objects.firstNonNull(newValue, ""));
 			}
 		});
 	}
-
+	
 }
