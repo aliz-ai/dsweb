6242297c0e8589ad98ee85e4281c05959dbae0bf
David Ipacs <david.ipacs@gmail.com>
2014-11-12 07:27:09 +0100

    TagsInput test commented out.

--- dsweb/src/test/java/com/doctusoft/dsw/client/gwt/TestInputTagsRenderer.java
+++ dsweb/src/test/java/com/doctusoft/dsw/client/gwt/TestInputTagsRenderer.java
@@ -1,49 +1,43 @@
 package com.doctusoft.dsw.client.gwt;
 
-import org.junit.Ignore;
-
-import com.doctusoft.bean.binding.observable.ObservableList;
-import com.doctusoft.dsw.client.comp.InputTags;
-import com.google.gwt.user.client.Timer;
-import com.xedge.jquery.client.JQuery;
 
 public class TestInputTagsRenderer extends AbstractDswebTest {
 	
 //	@Test
-	@Ignore
-	public void testDefaultTagsPresent() {
-		InputTags inputTags = new InputTags().withId("inputTags");
-		ObservableList<String> tagList = inputTags.getModel().getTagList();
-		tagList.add("a");
-//		tagList.add("b");
-//		tagList.add("c");
-		registerApp(inputTags);
-		new Timer() {
-			@Override
-			public void run() {
-				assertEquals(1, JQuery.select("#inputTags").length());
-				assertEquals(3, JQuery.select(".bootstrap-tagsinput").length());
-				finishTest();
-			}
-		}.schedule(200);
-		delayTestFinish(500);
-	}
+//	@Ignore
+//	public void testDefaultTagsPresent() {
+//		InputTags inputTags = new InputTags().withId("inputTags");
+//		ObservableList<String> tagList = inputTags.getModel().getTagList();
+//		tagList.add("a");
+////		tagList.add("b");
+////		tagList.add("c");
+//		registerApp(inputTags);
+//		new Timer() {
+//			@Override
+//			public void run() {
+//				assertEquals(1, JQuery.select("#inputTags").length());
+//				assertEquals(3, JQuery.select(".bootstrap-tagsinput").length());
+//				finishTest();
+//			}
+//		}.schedule(200);
+//		delayTestFinish(500);
+//	}
 	
 //	@Test
-	@Ignore
-	public void testWithPlaceHolder() {
-		InputTags inputTags = new InputTags()
-			.withId("inputTags")
-			.withPlaceHolder("ph1");
-		registerApp(inputTags);
-		new Timer() {
-			@Override
-			public void run() {
-				assertEquals("ph1", JQuery.select("#inputTags").attr("placeholder"));
-				finishTest();
-			}
-		}.schedule(200);
-		delayTestFinish(500);
-	}
+//	@Ignore
+//	public void testWithPlaceHolder() {
+//		InputTags inputTags = new InputTags()
+//			.withId("inputTags")
+//			.withPlaceHolder("ph1");
+//		registerApp(inputTags);
+//		new Timer() {
+//			@Override
+//			public void run() {
+//				assertEquals("ph1", JQuery.select("#inputTags").attr("placeholder"));
+//				finishTest();
+//			}
+//		}.schedule(200);
+//		delayTestFinish(500);
+//	}
 	
 }
