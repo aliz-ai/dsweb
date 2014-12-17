acd565bb07b60e211a238ff1d1275bfc70f1e3c1
gabor-farkas <gabor.farkas@doctusoft.com>
2014-12-17 12:35:26 +0100

    fixing TestTypeaheadRenderer.java

--- dsweb/src/test/java/com/doctusoft/dsw/client/gwt/TestTypeaheadRenderer.java
+++ dsweb/src/test/java/com/doctusoft/dsw/client/gwt/TestTypeaheadRenderer.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import com.doctusoft.dsw.client.comp.SelectItem;
 import com.doctusoft.dsw.client.comp.Typeahead;
+import com.doctusoft.dsw.client.util.GWTTimerDeferrerImpl;
+import com.google.gwt.user.client.Timer;
 import com.xedge.jquery.client.JQuery;
 
 @Log
@@ -17,6 +19,7 @@ public class TestTypeaheadRenderer extends AbstractDswebTest {
 	
 	@Test
 	public void testSelectItems() {
+		new GWTTimerDeferrerImpl();
 		Typeahead<String> typeahead = new Typeahead<String>().withId( "typeahead" ).withSelectItems( createDummySelectItems() );
 		registerApp( typeahead );
 		JQuery jqRoot = JQuery.select( ":root" );
@@ -40,17 +43,25 @@ public class TestTypeaheadRenderer extends AbstractDswebTest {
 	}
 	
 	@Test
-	public void testCostumTextTyping() {
-		Typeahead<String> typeahead = new Typeahead<String>().allowCustomText().withId("typeahead_costumtext").withSelectItems(createDummySelectItems());
+	public void testCustomTextTyping() {
+		new GWTTimerDeferrerImpl();
+		final Typeahead<String> typeahead = new Typeahead<String>().allowCustomText().withId("typeahead_costumtext").withSelectItems(createDummySelectItems());
 		registerApp(typeahead);
-		JQuery jqTypeahead = JQuery.select("#typeahead_costumtext");
-		jqTypeahead.val("Proba");
-		jqTypeahead.change();
-		assertEquals("Proba", typeahead.getModel().getCustomText());
-		assertEquals(-1, typeahead.getModel().getSelectedIndex());
-		jqTypeahead.val("1");
-		jqTypeahead.change();
-		assertEquals(null, typeahead.getModel().getCustomText());
-		assertEquals(0,typeahead.getModel().getSelectedIndex());
+		new Timer() {
+			@Override
+			public void run() {
+				JQuery jqTypeahead = JQuery.select("#typeahead_costumtext");
+				jqTypeahead.val("Proba");
+				jqTypeahead.change();
+				assertEquals("Proba", typeahead.getModel().getCustomText());
+				assertEquals(-1, typeahead.getModel().getSelectedIndex());
+				jqTypeahead.val("1");
+				jqTypeahead.change();
+				assertEquals(null, typeahead.getModel().getCustomText());
+				assertEquals(0,typeahead.getModel().getSelectedIndex());
+				finishTest();
+			}
+		}.schedule(25);
+		delayTestFinish(100);
 	}
 }
